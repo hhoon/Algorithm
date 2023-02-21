70bdf785796d146bebf1eb56899e741aea8e681f
hhoon <doubleh0423@gmail.com>
2023-02-22 00:33:34 +0900

    study Baekjoon 5086

---
+++ Baekjoon/5086.py
@@ -0,0 +1,11 @@
+while True :
+    a, b = map(int, input().split())
+
+    if a == 0 & b == 0 :
+        break
+    elif (b / a) == (b//a) :
+        print("factor")
+    elif (a / b) == (a//b) :
+        print("multiple")
+    else :
+        print("neither")
